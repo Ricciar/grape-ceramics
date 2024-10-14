ea473614f52d516d910673afbad2b02759ab3495
noraGrundberg <nora94grundberg@gmail.com>
2024-10-14 17:06:22 +0200

    create vite, reacte and tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+      "./index.html", // Om du har en index.html-fil
+      "./src/**/*.{js,ts,jsx,tsx}", // För alla JS, TS, JSX, TSX-filer i src-mappen
+   ],
+   theme: {
+      extend: {},
+   },
+   plugins: [],
+};
