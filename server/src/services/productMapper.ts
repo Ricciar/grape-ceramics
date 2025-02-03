c682103531467e9b00b18a9c1f929796724dc53f
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2025-02-03 15:44:43 +0100

    Add ProductMapper for handling WooCommerce product data

---
+++ server/src/services/productMapper.ts
@@ -0,0 +1,19 @@
+import { Product, ProductResponse } from '@/controllers/types/product.types';
+import { stripHtml } from '@/utils/productUtils';
+
+// Tar in rådata från API
+export class ProductMapper {
+  mapProduct(product: ProductResponse): Product {
+    return {
+      id: product.id,
+      name: product.name,
+      images: product.images.map((image) => image.src),
+      description: stripHtml(product.description),
+      regular_price: product.regular_price,
+      sale_price: product.sale_price,
+      price: product.price,
+      stock_status: product.stock_status,
+      stock_quantity: product.stock_quantity,
+    };
+  }
+}
