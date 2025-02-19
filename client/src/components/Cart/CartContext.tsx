0a69bab258f2450050bd71b6c86a7f80628f4d11
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2025-02-19 16:24:49 +0100

    update interface CartItem

--- client/src/components/Cart/CartContext.tsx
+++ client/src/components/Cart/CartContext.tsx
@@ -1,69 +1,70 @@
-import React, { createContext, useState, ReactNode } from "react";
+import React, { createContext, useState, ReactNode } from 'react';
 
 // Definiera en typ för produkten i varukorgen
 export interface CartItem {
-   id: number;
-   name: string;
-   price: string;
-   quantity: number;
-   imageUrl: string;
-   description: string;
+  id: string | number;
+  name: string;
+  price: string;
+  quantity: number;
+  imageUrl: string;
+  description: string;
 }
 
 // Definiera kontextens struktur
 export type CartContextType = {
-   cart: CartItem[];
-   addToCart: (product: CartItem, quantityChange: number) => void;
-   removeFromCart: (productId: number) => void;
-   clearCart: () => void;
+  cart: CartItem[];
+  addToCart: (product: CartItem, quantityChange: number) => void;
+  removeFromCart: (productId: number) => void;
+  clearCart: () => void;
 };
 
 // Skapa en kontext med standardvärden
 export const CartContext = createContext<CartContextType | undefined>(
-   undefined
+  undefined
 );
 
 // Provider-komponenten som håller i varukorgens state
 export const CartProvider: React.FC<{ children: ReactNode }> = ({
-   children,
+  children,
 }) => {
-   const [cart, setCart] = useState<CartItem[]>([]);
+  const [cart, setCart] = useState<CartItem[]>([]);
 
-   const addToCart = (product: CartItem, quantityChange: number = 1) => {
-      console.log("addToCart called with quantityChange:", quantityChange); // Logga varje anrop
-      setCart((prevCart) => {
-         const existingItemIndex = prevCart.findIndex(
-            (item) => item.id === product.id
-         );
+  const addToCart = (product: CartItem, quantityChange: number = 1) => {
+    console.log('addToCart called with quantityChange:', quantityChange); // Logga varje anrop
+    setCart((prevCart) => {
+      const existingItemIndex = prevCart.findIndex(
+        (item) => item.id === product.id
+      );
 
-         if (existingItemIndex >= 0) {
-            // Om produkten redan finns i varukorgen, uppdatera dess kvantitet
-            const updatedCart = [...prevCart];
-            updatedCart[existingItemIndex].quantity += quantityChange;
+      if (existingItemIndex >= 0) {
+        // Om produkten redan finns i varukorgen, uppdatera dess kvantitet
+        const updatedCart = [...prevCart];
+        updatedCart[existingItemIndex].quantity += quantityChange;
 
-            // Om kvantiteten blir noll eller mindre, ta bort produkten från varukorgen
-            if (updatedCart[existingItemIndex].quantity <= 0) {
-               return updatedCart.filter((item) => item.id !== product.id);
-            }
+        // Om kvantiteten blir noll eller mindre, ta bort produkten från varukorgen
+        if (updatedCart[existingItemIndex].quantity <= 0) {
+          return updatedCart.filter((item) => item.id !== product.id);
+        }
 
-            return updatedCart;
-         } else {
-            // Om produkten inte finns i varukorgen, lägg till den med startkvantitet
-            return [...prevCart, { ...product, quantity: quantityChange }];
-         }
-      });
-   };
+        return updatedCart;
+      } else {
+        // Om produkten inte finns i varukorgen, lägg till den med startkvantitet
+        return [...prevCart, { ...product, quantity: quantityChange }];
+      }
+    });
+  };
 
-   const removeFromCart = (productId: number) => {
-      setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
-   };
+  const removeFromCart = (productId: number) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+  };
 
-   const clearCart = () => setCart([]);
+  const clearCart = () => setCart([]);
 
-   return (
-      <CartContext.Provider
-         value={{ cart, addToCart, removeFromCart, clearCart }}>
-         {children}
-      </CartContext.Provider>
-   );
+  return (
+    <CartContext.Provider
+      value={{ cart, addToCart, removeFromCart, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 };
