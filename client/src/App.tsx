99b4b3e994f993be910305eb87a1b3cdf0f60349
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2025-02-19 16:24:17 +0100

    feat: import of new ProductDetail component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,27 @@
-import "./App.css";
-import { CartProvider } from "./components/Cart/CartContext";
-import Navbar from "./components/Navbar/Navbar";
-import ProductPageDetail from "./pages/ProductPageDetail";
-import ShopGrid from "./pages/ShopGrid";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './components/Cart/CartContext';
+import Navbar from './components/Navbar/Navbar';
+//import ProductPageDetail from './pages/ProductPageDetail';
+import ShopGrid from './pages/ShopGrid';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
 
-function App() {
-   return (
-      <CartProvider>
-         <Router>
-            <Navbar />
-            <Routes>
-               {/* Route för produktlistan */}
-               <Route path="/" element={<ShopGrid />} />
+function App(): JSX.Element {
+  return (
+    <CartProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* Home page - Product listing */}
+          <Route path="/" element={<ShopGrid />} />
 
-               {/* Route för produktsidan, med dynamisk produkt-ID */}
-               <Route path="/product/:id" element={<ProductPageDetail />} />
-            </Routes>
-         </Router>
-      </CartProvider>
-   );
+          {/* Route för produktsidan, med dynamisk produkt-ID */}
+          <Route path="/product/:id" element={<ProductDetail />} />
+        </Routes>
+      </Router>
+    </CartProvider>
+  );
 }
 
 export default App;
