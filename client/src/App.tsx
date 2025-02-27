99df7c5bac535f423fa57b79cd18dfb1a8df3563
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2025-02-27 10:28:35 +0100

    feat(routing): Implement proper route structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { CartProvider } from './components/Cart/CartContext';
 import Navbar from './components/Navbar/Navbar';
-//import ProductPageDetail from './pages/ProductPageDetail';
 import ShopGrid from './pages/ShopGrid';
-import ProductDetail from './pages/ProductDetail/ProductDetail';
+import ProductWrapper from './pages/ProductWrapper';
 
 function App(): JSX.Element {
   return (
@@ -13,11 +17,16 @@ function App(): JSX.Element {
       <Router>
         <Navbar />
         <Routes>
-          {/* Home page - Product listing */}
-          <Route path="/" element={<ShopGrid />} />
+          {/* <Route path="/" element={<Home />} */}
+
+          {/* Redirect från home till shop */}
+          <Route path="/" element={<Navigate to="/shop" replace />} />
+
+          {/* Shop page - Product listing */}
+          <Route path="/shop" element={<ShopGrid />} />
 
           {/* Route för produktsidan, med dynamisk produkt-ID */}
-          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/product/:id" element={<ProductWrapper />} />
         </Routes>
       </Router>
     </CartProvider>
