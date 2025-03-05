efdca9b150513bcf6c0142db3a731810ee9fee86
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2025-03-05 14:23:20 +0100

    feat: Create DesktopProductCard component for larger screens

---
+++ client/src/pages/shopgrid/DesktopProductCard.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { DesktopProductCardProps } from './types';
+import { capitalizeFirstLetter } from './ProductLayoutUtils';
+
+const DesktopProductCard: React.FC<DesktopProductCardProps> = ({
+  product,
+  onClick,
+}) => {
+  return (
+    <div className="cursor-pointer" onClick={() => onClick(product.id)}>
+      <div className="w-full h-[450px] bg-gray-100 overflow-hidden">
+        <img
+          src={product.images.at(0)}
+          alt={product.name}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      {/* Produktnamn och pris */}
+      <div className="mt-2 ml-3 flex flex-col justify-between">
+        <span className="text-xs font-light leading-none tracking-[2.28px] text-[#1C1B1F]">
+          {capitalizeFirstLetter(product.name)}
+        </span>
+        <div className="flex text-right font-light mr-2 mt-2 mb-2">
+          {product.sale_price ? (
+            <>
+              <span className="text-xs line-through text-gray-400">
+                {product.regular_price} SEK
+              </span>
+              <br />
+              <span className="text-xs ml-2">{product.price} SEK</span>
+            </>
+          ) : (
+            <span className="text-xs font-light leading-none tracking-[2.28px] text-[#1C1B1F]">
+              {product.price} SEK
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DesktopProductCard;
