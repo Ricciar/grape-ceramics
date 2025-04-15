d279d25d318c7cdfe44fd18e96c6e8e891288975
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2025-04-15 15:33:08 +0200

    feat: add navbar to component

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,31 +1,35 @@
-import React, { useState } from "react";
-import CartIcon from "./CartIcon";
-import CartPage from "../Cart/CartPage";
+import React, { useState } from 'react';
+import CartIcon from './CartIcon';
+import CartPage from '../Cart/CartPage';
+import NavMenu from './NavMenu';
 
 const Navbar: React.FC = () => {
-   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
-   return (
-      <nav className="mx-auto min-w-[319px] max-w-md flex justify-between items-center p-4 bg-[#F8F4EC]">
-         {/* Logo */}
-         <h1
-            className="flex flex-col justify-center align-middle
-       text-[24px] tracking-[4.37px]">
-            GRAPE{" "}
-            <span className="text-[14px] tracking-[2.66px]">
-               <br />
-               CERAMICS
-            </span>
-         </h1>
-         {/* Cart Icon */}
-         <button onClick={() => setIsCartOpen(true)}>
-            <CartIcon />
-         </button>
+  return (
+    <nav className="mx-auto min-w-[319px] max-w-md flex justify-between items-center p-4 bg-[#F8F4EC]">
+      {/* Hamburger Menu */}
+      <NavMenu />
+      {/* Logo */}
+      <h1
+        className="flex flex-col justify-center align-middle
+       text-[24px] tracking-[4.37px]"
+      >
+        GRAPE{' '}
+        <span className="text-[14px] tracking-[2.66px]">
+          <br />
+          CERAMICS
+        </span>
+      </h1>
+      {/* Cart Icon */}
+      <button onClick={() => setIsCartOpen(true)}>
+        <CartIcon />
+      </button>
 
-         {/* Cart Page */}
-         <CartPage isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
-      </nav>
-   );
+      {/* Cart Page */}
+      <CartPage isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
+    </nav>
+  );
 };
 
 export default Navbar;
