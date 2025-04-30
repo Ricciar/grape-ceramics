15f24ac2a63393821bd3950ae092529e425c8df0
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2025-04-30 20:00:35 +0200

    refactor: update ProductSkeleton to match ProductDetail layout

--- client/src/pages/ProductDetail/ProductSkeleton.tsx
+++ client/src/pages/ProductDetail/ProductSkeleton.tsx
@@ -1,35 +1,39 @@
 import React from 'react';
 
 const ProductSkeleton: React.FC = () => (
-  <div className="animate-pulse flex flex-col items-center md:flex-row md:justify-around md:gap-[11rem] mx-auto min-w-[319px] max-w-md md:max-w-[1200px] pl-[1px] pr-[1px] pt-0">
+  <div className="flex flex-col items-center lg:flex-row lg:justify-around lg:gap-[11rem]">
     {/* Vänster sektion med bild och galleri */}
-    <div className="w-full md:w-1/2 flex flex-col">
-      {/* Huvudbild skeleton - uppdaterad med korrekta dimensioner */}
-      <div className="relative w-full md:w-[600px] h-[450px] md:h-[645px] bg-gray-200" />
+    <div className="w-full lg:w-1/2 flex flex-col">
+      {/* Huvudbild skeleton */}
+      <div className="relative w-full lg:w-[600px] h-[450px] lg:h-[645px] bg-gray-200" />
 
-      {/* Miniatyrbilder skeleton - uppdaterad med korrekt spacing */}
-      <div className="flex mt-1 self-start space-x-1">
-        {[1, 2, 3].map((i) => (
+      {/* Miniatyrbilder skeleton */}
+      <div className="flex mt-2 space-x-2 overflow-x-auto">
+        {[1, 2, 3, 4].map((i) => (
           <div
             key={i}
-            className="min-w-[44px] h-11 md:w-[90px] md:h-[90px] bg-gray-200"
+            className="min-w-[75px] w-[75px] h-[75px] lg:w-[90px] lg:h-[90px] bg-gray-200 flex-shrink-0"
           />
         ))}
       </div>
     </div>
 
-    {/* Höger sektion med produktinfo - uppdaterad med korrekt styling */}
-    <div className="flex flex-col items-center w-full md:w-1/2 md:items-start max-w-[290px] md:max-w-none font-light tracking-[2.85px]">
+    {/* Höger sektion med produktinfo */}
+    <div className="flex flex-col items-center w-full lg:w-1/2 lg:items-start max-w-[290px] lg:max-w-none font-light tracking-[2.85px]">
       {/* Titel skeleton */}
-      <div className="w-3/4 h-6 bg-gray-200 mt-5" />
+      <div className="w-3/4 h-6 bg-gray-200 mt-5 self-center lg:self-start" />
 
       {/* Beskrivning skeleton */}
-      <div className="w-full h-20 bg-gray-200 mt-[5px]" />
+      <div className="w-full h-28 bg-gray-200 mt-[5px]" />
 
       {/* Pris och lager skeleton */}
       <div className="w-full h-[65px] flex justify-between mt-4 mb-2">
-        <div className="w-1/3 h-4 bg-gray-200" />
-        <div className="w-1/4 h-4 bg-gray-200" />
+        <div className="flex items-center">
+          <div className="w-24 h-4 bg-gray-200" />
+        </div>
+        <div className="text-right">
+          <div className="w-16 h-4 bg-gray-200 ml-auto" />
+        </div>
       </div>
 
       {/* Knapp skeleton */}
