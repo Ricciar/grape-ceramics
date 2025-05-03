b41593ec857c747fc255551d5fb476275777f638
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2025-05-03 18:19:50 +0200

    fix: correct CartItem import from Navbar/CartIcon

--- client/src/components/Cart/CheckoutRedirect.tsx
+++ client/src/components/Cart/CheckoutRedirect.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../Button';
 import axios from 'axios';
-import { CartItem } from '../Navbar/CartIcon';
+import { CartItem } from '../Cart/CartContext';
 
 interface CheckoutRedirectProps {
   cart: CartItem[]; // Varukorgen
